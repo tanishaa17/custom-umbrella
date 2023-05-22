81e6c1c2234603279ce9456c20d2e19cc8fcf9db
tanishaa17 <krati1701@gmail.com>
2023-05-23 03:50:33 +0530

    Added CSS and fixed bugs

--- index.js
+++ index.js
@@ -4,10 +4,28 @@ const changeColor = (color) => {
 
 }
 
-const ButtonColor = document.getElementsByClassName("ButtonColor")
 
-for (let i = 0; i < ButtonColor.length; i++) {
-    ButtonColor[i].addEventListener("click", (e) => {
+const colorDiv = document.getElementById("colorDiv");
+
+const btn1 = document.createElement("button");
+btn1.setAttribute("id", "blue")
+btn1.setAttribute("class", "buttonColor")
+const btn2 = document.createElement("button");
+btn2.setAttribute("id", "pink")
+btn2.setAttribute("class", "buttonColor")
+const btn3 = document.createElement("button");
+btn3.setAttribute("id", "yellow")
+btn3.setAttribute("class", "buttonColor")
+
+
+colorDiv.append(btn1, btn2, btn3);
+
+
+
+const buttonColor = document.getElementsByClassName("buttonColor")
+
+for (let i = 0; i < buttonColor.length; i++) {
+    buttonColor[i].addEventListener("click", (e) => {
         const color = e.target.id
         changeColor(color)
     })
